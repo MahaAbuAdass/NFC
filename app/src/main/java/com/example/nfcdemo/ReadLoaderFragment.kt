5a7437089cefebcac860f5d1aa28e24c8d5b56a0
maha <mahaabuadass@gmail.com>
2024-07-16 15:55:20 +0300

    third commit

--- app/src/main/java/com/example/nfcdemo/ReadLoaderFragment.kt
+++ app/src/main/java/com/example/nfcdemo/ReadLoaderFragment.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.nfc.tech.Ndef
+import android.nfc.tech.*
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -13,12 +13,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.nfcdemo.databinding.LoaderReadFragmentBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.io.IOException
 
 class ReadLoaderFragment : Fragment(), NfcFragment {
 
@@ -28,6 +28,10 @@ class ReadLoaderFragment : Fragment(), NfcFragment {
     private val handler = Handler(Looper.getMainLooper())
     private val timeoutMillis: Long = 10000 // 10 seconds timeout
     private var isTagDetected = false
+
+    companion object {
+        private const val TAG = "NFC TAG"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,7 +68,7 @@ class ReadLoaderFragment : Fragment(), NfcFragment {
     }
 
     override fun onNfcIntent(intent: Intent) {
-        Log.d(TAG, "onNfcIntent")
+        Log.v(TAG, "onNfcIntent")
 
         if (NfcAdapter.ACTION_TAG_DISCOVERED == intent.action) {
             val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
@@ -74,33 +78,111 @@ class ReadLoaderFragment : Fragment(), NfcFragment {
             handler.removeCallbacks(timeoutRunnable) // Stop timeout countdown
             hideBottomSheet(true)
 
-            // Handle NDEF messages
-            handleNdefTag(tag)
+            // Handle the tag
+            handleTag(tag)
         }
     }
 
-    private fun handleNdefTag(tag: Tag?) {
-        Log.d(TAG, "handle tag")
-
+    private fun handleTag(tag: Tag?) {
         tag ?: return
+
+        val techList = tag.techList ?: return  // Ensure techList is not null
+
+        for (tech in techList) {
+            when (tech) {
+                Ndef::class.java.name -> handleNdefTag(tag)
+                NfcA::class.java.name -> handleNfcATag(tag)
+                NfcB::class.java.name -> handleNfcBTag(tag)
+                NfcF::class.java.name -> handleNfcFTag(tag)
+                NfcV::class.java.name -> handleNfcVTag(tag)
+                IsoDep::class.java.name -> handleIsoDepTag(tag)
+                MifareClassic::class.java.name -> handleMifareClassicTag(tag)
+                MifareUltralight::class.java.name -> handleMifareUltralightTag(tag)
+                else -> handleUnknownTag(tag, tech)
+            }
+        }
+    }
+
+    private fun handleNdefTag(tag: Tag) {
+        Log.v(TAG, "handleNdefTag")
 
         val ndef = Ndef.get(tag)
         ndef?.let {
             try {
                 it.connect()
                 val message = it.ndefMessage
-                Log.d(TAG, "NDEF Message: ${message?.toString()}")
+                Log.v(TAG, "NDEF Message: ${message?.toString()}")
+                Toast.makeText(requireContext(), "NDEF Tag detected", Toast.LENGTH_SHORT).show()
+
                 // Process the NDEF message further as needed
             } catch (e: Exception) {
-                Log.e(TAG, "Error reading NFC tag", e)
+                Log.v(TAG, "Error reading NFC tag", e)
                 Toast.makeText(requireContext(), "Error reading NFC tag", Toast.LENGTH_SHORT).show()
             } finally {
-                it.close()
+                try {
+                    it.close()
+                } catch (e: IOException) {
+                    Log.v(TAG, "Error closing Ndef", e)
+                }
             }
         } ?: run {
-            Log.e(TAG, "NDEF is null - Tag is not NDEF formatted or NDEF is not supported.")
+            Log.v(TAG, "NDEF is null - Tag is not NDEF formatted or NDEF is not supported.")
             Toast.makeText(requireContext(), "Tag is not NDEF formatted or NDEF is not supported.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun handleNfcATag(tag: Tag) {
+        Log.v(TAG, "handleNfcATag")
+        // Implement handling of NFC-A tags
+        Toast.makeText(requireContext(), "NFC-A Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleNfcBTag(tag: Tag) {
+        Log.v(TAG, "handleNfcBTag")
+        // Implement handling of NFC-B tags
+        Toast.makeText(requireContext(), "NFC-B Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleNfcFTag(tag: Tag) {
+        Log.v(TAG, "handleNfcFTag")
+        // Implement handling of NFC-F tags
+        Toast.makeText(requireContext(), "NFC-F Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleNfcVTag(tag: Tag) {
+        Log.v(TAG, "handleNfcVTag")
+        // Implement handling of NFC-V tags
+        Toast.makeText(requireContext(), "NFC-V Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleIsoDepTag(tag: Tag) {
+        Log.v(TAG, "handleIsoDepTag")
+        // Implement handling of IsoDep tags
+        Toast.makeText(requireContext(), "IsoDep Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleMifareClassicTag(tag: Tag) {
+        Log.v(TAG, "handleMifareClassicTag")
+        // Implement handling of MifareClassic tags
+        Toast.makeText(requireContext(), "Mifare Classic Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleMifareUltralightTag(tag: Tag) {
+        Log.v(TAG, "handleMifareUltralightTag")
+        // Implement handling of MifareUltralight tags
+        Toast.makeText(requireContext(), "Mifare Ultralight Tag detected", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleUnknownTag(tag: Tag, technology: String) {
+        Log.v(TAG, "handleUnknownTag: $technology")
+        Toast.makeText(requireContext(), "Unknown tag technology: $technology", Toast.LENGTH_SHORT).show()
     }
 
     private val timeoutRunnable = Runnable {
@@ -110,8 +192,6 @@ class ReadLoaderFragment : Fragment(), NfcFragment {
     }
 
     private fun enableNfc() {
-        Log.d(TAG, "enable nfc Message")
-
         val intent = Intent(requireContext(), requireActivity().javaClass).apply {
             addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         }
@@ -119,55 +199,50 @@ class ReadLoaderFragment : Fragment(), NfcFragment {
             requireContext(),
             0,
             intent,
-            PendingIntent.FLAG_IMMUTABLE  // Use FLAG_IMMUTABLE for Android S and above
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
-        val intentFilter = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
-        val techList = arrayOf(arrayOf(Ndef::class.java.name)) // Adjust as per your supported tech list
-        val intentFiltersArray = arrayOf(intentFilter)
-        nfcAdapter?.enableForegroundDispatch(requireActivity(), pendingIntent, intentFiltersArray, techList)
+        val filter = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+
+        val intentFiltersArray = arrayOf(filter)
+        val techListsArray = arrayOf(
+            arrayOf(Ndef::class.java.name),
+            arrayOf(NfcA::class.java.name),
+            arrayOf(NfcB::class.java.name),
+            arrayOf(NfcF::class.java.name),
+            arrayOf(NfcV::class.java.name),
+            arrayOf(IsoDep::class.java.name),
+            arrayOf(MifareClassic::class.java.name),
+            arrayOf(MifareUltralight::class.java.name)
+        )
+
+        nfcAdapter?.enableForegroundDispatch(
+            requireActivity(),
+            pendingIntent,
+            intentFiltersArray,
+            techListsArray
+        )
     }
 
     private fun disableNfc() {
-        Log.d(TAG, "disable nfc Message")
-
         nfcAdapter?.disableForegroundDispatch(requireActivity())
     }
 
     private fun showBottomSheet() {
-        Log.d(TAG, "Showing bottom sheet...")
-        val view = layoutInflater.inflate(R.layout.bottom_sheet_progress, null)
         bottomSheetDialog = BottomSheetDialog(requireContext())
-        bottomSheetDialog?.setContentView(view)
+        bottomSheetDialog?.setContentView(R.layout.bottom_sheet_progress)
+        bottomSheetDialog?.setCancelable(false)
         bottomSheetDialog?.show()
-
-        // Set cancel button listener
-        view.findViewById<Button>(R.id.cancelLoaderButton).setOnClickListener {
-            handler.removeCallbacks(timeoutRunnable) // Stop timeout countdown
-            hideBottomSheet(false)
-            requireActivity().onBackPressed()
-        }
     }
 
-    private fun hideBottomSheet(isSuccess: Boolean) {
-        Log.d(TAG, "Hiding bottom sheet...")
+    private fun hideBottomSheet(success: Boolean) {
         bottomSheetDialog?.dismiss()
-
-        if (isSuccess) {
-            Toast.makeText(requireContext(), "Read successful!", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(requireContext(), "Read failed. Please try again.", Toast.LENGTH_SHORT).show()
+        if (!success) {
+            Toast.makeText(requireContext(), "NFC tag reading timed out", Toast.LENGTH_SHORT).show()
+            navigateBack()
         }
-
-        requireActivity().onBackPressed()
     }
 
     private fun navigateBack() {
-        if (findNavController().currentDestination?.id == R.id.readFragment) {
-            findNavController().popBackStack()
-        }
-    }
-
-    companion object {
-        private const val TAG = "ReadLoaderFragment"
+        findNavController().navigateUp()
     }
 }
